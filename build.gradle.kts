107c039a34c614e64edf8046f267ebe362ea5b8c
DomamaN202 <vip.domaman@mail.ru>
2025-11-19 06:19:15 +0700

    Подготовка к использованию

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,3 +21,49 @@ dependencies {
 tasks.test {
     useJUnitPlatform()
 }
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+
+            pom {
+                name.set("Bytecode Utils Library")
+                description.set("Java bytecode manipulation library.")
+                url.set("https://github.com/Domaman202/BytecodeUtilsLibrary")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("DomamaN202")
+                        name.set("DomamaN202")
+                        email.set("vip.domaman@mail.ru")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/Domaman202/BytecodeUtilsLibrary.git")
+                    developerConnection.set("scm:git:ssh://github.com/Domaman202/BytecodeUtilsLibrary.git")
+                    url.set("https://github.com/Domaman202/BytecodeUtilsLibrary")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            name = "OSSRH"
+            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_PASSWORD")
+            }
+        }
+    }
+}
